79b1dd950e7c8cd91c982c1549e5871f12eb94b1
Sergey Malenko <Sergey.Malenko@x5.ru>
2021-08-25 17:52:44 +0300

    tests

--- server/apps/user/models.py
+++ server/apps/user/models.py
@@ -7,3 +7,9 @@ class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(64), nullable=False)
     password = db.Column(db.String(32), nullable=False)
+
+
+class Session(db.Model):
+    __tablename__ = "session"
+    id = db.Column(db.String(32), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id"), nullable=False)
